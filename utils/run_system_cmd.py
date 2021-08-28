d85e212c5915e4a3bbca0397ead3426d20506613
Aleksandr Khaikin <aleksandr.sakh@gmail.com>
2021-08-28 16:19:14 +0700

    Added application launch in case of an error

--- utils/run_system_cmd.py
+++ utils/run_system_cmd.py
@@ -1,10 +1,12 @@
 import subprocess
+import config
 from utils.utils import write_log
 from utils.send_mail import send_mail
 
 
 def run_system_cmd(cmd, desc):
     if subprocess.call(cmd, shell=True) != 0:
+        subprocess.call(config.COMMAND_START, shell=True)
         msg = f'An error occurred while {desc}'
         send_mail(msg, 'error')
         write_log(msg)
